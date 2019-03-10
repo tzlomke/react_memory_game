cfe324fec9c624355fcb24f4d3a4b8ab41db7443
Chieftjz <taylor.zlomke@gmail.com>
2019-03-09 22:03:37 -0700

    Updates

---
+++ precache-manifest.c6c94ab8bc9e0f1947e2a7c9ca3131af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e51b1dc27f775a2c12d1",
+    "url": "/react_memory_game/static/js/runtime~main.3ed987ac.js"
+  },
+  {
+    "revision": "2470801d97427aa3aa6e",
+    "url": "/react_memory_game/static/js/main.60349df0.chunk.js"
+  },
+  {
+    "revision": "a539ea9d3b1acab9c730",
+    "url": "/react_memory_game/static/js/2.948d4d77.chunk.js"
+  },
+  {
+    "revision": "2470801d97427aa3aa6e",
+    "url": "/react_memory_game/static/css/main.c8b9ba66.chunk.css"
+  },
+  {
+    "revision": "344f6443fb258ee62d123359aa037588",
+    "url": "/react_memory_game/index.html"
+  }
+];
