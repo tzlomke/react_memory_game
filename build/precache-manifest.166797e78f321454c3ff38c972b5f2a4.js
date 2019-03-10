f78428545cb0302efbae66b00634ed8087b55913
Chieftjz <taylor.zlomke@gmail.com>
2019-03-10 14:21:11 -0600

    updated readme

--- build/precache-manifest.166797e78f321454c3ff38c972b5f2a4.js
+++ build/precache-manifest.166797e78f321454c3ff38c972b5f2a4.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/react_memory_game/static/js/runtime~main.3ed987ac.js"
   },
   {
-    "revision": "48fed14cb26964a31535",
-    "url": "/react_memory_game/static/js/main.0243a6f6.chunk.js"
+    "revision": "535c70c16d874dda5340",
+    "url": "/react_memory_game/static/js/main.0f876dd1.chunk.js"
   },
   {
     "revision": "a539ea9d3b1acab9c730",
     "url": "/react_memory_game/static/js/2.948d4d77.chunk.js"
   },
   {
-    "revision": "48fed14cb26964a31535",
+    "revision": "535c70c16d874dda5340",
     "url": "/react_memory_game/static/css/main.c8b9ba66.chunk.css"
   },
   {
-    "revision": "c3bde811bfba436615efb0b0687f9ec3",
+    "revision": "aa2b7fe9fd4553d43862d8a689932461",
     "url": "/react_memory_game/index.html"
   }
 ];
